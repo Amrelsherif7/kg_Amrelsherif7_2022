0f1c1735af206683edbf86e35a08507141195086
Amr Elsherif <78764832+Amrelsherif7@users.noreply.github.com>
2021-04-26 22:19:11 -0400

    Python language
    Python language

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#Python Program to convert List of Integers into String
 D={0:'Zero',1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine'}
 def conver(the_list):
     List = the_list
